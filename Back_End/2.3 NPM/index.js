369d893db6c2c7381914fae7037fc61285bf85a8
Kabelo Mokhethi <kmokheth@gcn.ab-inbev.com>
2025-01-21 10:38:27 +0200

    added superheroes module to explore futher

--- Back_End/2.3 NPM/index.js
+++ Back_End/2.3 NPM/index.js
@@ -1,5 +1,10 @@
 import generateName from 'sillyName';
+import {randomSuperhero} from 'superheroes';
 
 const myName = generateName();
 
 console.log(`My silly name is ${myName}`);
+
+const mySuperHeroName = randomSuperhero();
+
+console.log(`My random super hero name is ${mySuperHeroName}`);
